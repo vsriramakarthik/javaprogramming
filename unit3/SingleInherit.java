3205ae041d6cc6c4ec8a9759d5596149dad2d9de
vsriramakarthik <sriramakarthik.vadlamudi@sasi.ac.in>
2023-10-27 13:37:03 +0530

    Update SingleInherit.java

--- unit3/SingleInherit.java
+++ unit3/SingleInherit.java
@@ -1,7 +1,13 @@
-class SingleInherit{
-    int a=30;
-    int b=40;
-    void display(){
+class SingleInherit {
+    int a = 30;
+    int b = 40;
+
+    void display() {
         System.out.println("i am a method from class parent class");
+
     }
+}
+
+class Demo extends SingleInherit {
+
 }
